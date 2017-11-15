3765bac9fb0280cbe4ff0bb3a2ec1c58475e4368
Truman Lahr <trumanlahr@gmail.com>
2017-11-15 15:48:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,9 +3,10 @@ artist: 'song title'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
-  return playlist
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
+  return playlist;
 }
